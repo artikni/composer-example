6161c7d6bfd51ced03b7fab4f8f972fee91301c7
Artyom Ni <artyom.ni@gmail.com>
2019-09-30 13:09:02 +0600

    Added files

---
+++ src/Parser.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace artyomsky\parser;
+
+/* 
+ * Parser Class, author Artyom Ni artyom.ni@gmail.com
+ */
+
+class Parser implements ParserInterface{
+    public function process(string $url, string $tag){
+        return [
+            'just',
+            'do',
+            'it'
+        ];
+    }
+}
+
+
